439a20b5a98b3a4b733ea31fd26d0007ad6723aa
Marian Zburlea <marianzburlea@gmail.com>
2018-11-07 22:08:10 +0000

    Updates

---
+++ precache-manifest.fe8da30ce1e980eee75540781f358d26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8afc5055985c2689183c",
+    "url": "/static/js/main.8afc5055.chunk.js"
+  },
+  {
+    "revision": "0cdd6f184db929cffcf5",
+    "url": "/static/js/1.0cdd6f18.chunk.js"
+  },
+  {
+    "revision": "8afc5055985c2689183c",
+    "url": "/static/css/main.6bdbf043.chunk.css"
+  },
+  {
+    "revision": "2832a06be0163ecb0ffa074609d406be",
+    "url": "/index.html"
+  }
+];
